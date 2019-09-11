5a867ca787652c6ad16b4c3493eadc1b4cd82b7c
Marek Mahdal <marek.mahdal@MMahdalMBA.local>
2019-09-11 13:01:27 +0200

    new build

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 // extension attached to URL links
 // $ext = ".php"; // if you use php server for testing
-$ext = ".php"; // if you use html server that does not support rewriting, e.g. Web Server Chrome Extension
+$ext = ".html"; // if you use html server that does not support rewriting, e.g. Web Server Chrome Extension
 // $ext = ""; // no extension for live production deployement
 
 $url = "https://alotops.com";
