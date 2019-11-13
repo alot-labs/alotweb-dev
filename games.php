dfbf04642d5e3f70cbb5b4941e691e506442da41
Marek Mahdal <marek.mahdal@MMahdalMBA.local>
2019-11-13 15:23:38 +0100

    Another revamp

--- games.php
+++ games.php
@@ -1,11 +1,11 @@
 <?php 
     include 'config.php';
 
-    $title = "ALOT Content";
+    $title = "ALOT Games";
     $meta_desc = "Unique portfolio of games and gaming content with huge jackpot prizes, including lottery betting, fixed odds lottery games, KENO, bingo, scratch cards and more.";
     $meta_keywords = "Lottery, huge jackpot, jackpot games, KENO, bingo, scratch cards, lottery betting";
     $og_image = "";
-    $og_url = $url."/game-content".$ext;
+    $og_url = $url."/games".$ext;
 
     include 'header.php';
     include 'menu.php';
@@ -16,7 +16,7 @@
         <div class="splitbox__image splitbox__image--lotto-betting"></div>
         <div class="splitbox__overlay"></div>
         <div class="splitbox__text">
-            <h2 class="heading-1">Lotto Betting</h2>
+            <h2 class="heading-1">Lotto</h2>
             <p class="splitbox__text-description">Demo implementation</p>
         </div>
     </a>
@@ -24,15 +24,15 @@
         <div class="splitbox__image splitbox__image--instant-games"></div>
         <div class="splitbox__overlay"></div>
         <div class="splitbox__text">
-            <h2 class="heading-1">Instant Games</h2>
+            <h2 class="heading-1">Instant</h2>
             <p class="splitbox__text-description">Demo implementation</p>
         </div>
     </a>
-    <a class="splitbox__item" href="numbers<?=$ext?>">
+    <a class="splitbox__item" href="hfg<?=$ext?>">
         <div class="splitbox__image splitbox__image--keno-betting"></div>
         <div class="splitbox__overlay"></div>
         <div class="splitbox__text">
-            <h2 class="heading-1">Numbers</h2>
+            <h2 class="heading-1">High Frequency</h2>
             <p class="splitbox__text-description">Demo implementation</p>
         </div>
     </a>
@@ -48,7 +48,7 @@
                 <p class="paragraph">Plug in our proprietary content catalogue to offer your players a winning experience. Some games also benefit from access to our prize cover, so you can offer high jackpot games with the pay-out risk taken by us.</p>
                 <p class="paragraph">Let your players bet on the world’s largest lottery jackpots and other popular lottery-style games. A great acquisition and retention product, but also an amazing revenue generator in its own right.</p>
                 <p class="paragraph">Prizes are guaranteed by our superior Jackpot Prize Cover, so you can rest assured that the payout is guaranteed, and you can focus on your business as&nbsp;usual.</p>
-                <a href="lotto-betting<?=$ext?>" class="button button--green">Lottery demo</a>
+                <a href="lotto-betting<?=$ext?>" class="button button--green">Lotto betting demo</a>
             </div>
         </div>
         <!--div class="heading-holder heading-holder--narrow-text">
@@ -196,7 +196,7 @@
     <div class="container">
         <div class="other-games__holder">
             <div class="other-games__column">
-                <h2 class="heading-1">Instant Win Games</h2>
+                <h2 class="heading-1">Instant</h2>
                 <div class="other-games__item">
                     <div class="other-games__illustration" style="background-image: url(i/instant-game.jpg);"></div>
                     <p class="paragraph">Integrate our Instant Win Games and other lottery-style games to your gaming portfolio. Our proprietary content lets your players watch the draw and win instantly. With a higher RTP than lottery betting our instant games are ideal for sportsbook and casino players. All of our games are iTech certified and powered by RiNG™ our unique Random & Insured Number Generator.</p>
@@ -204,11 +204,11 @@
                 </div>
             </div>
             <div class="other-games__column other-games__column--offset">
-                <h2 class="heading-1">Numbers</h2>
+                <h2 class="heading-1">High Frequency</h2>
                 <div class="other-games__item">
                     <div class="other-games__illustration" style="background-image: url(i/keno-game.jpg);"></div>
                     <p class="paragraph">Many players like to place traditional fixed odds bets on specific numbers (instead of picking all numbers like traditional lottery). let your players bet on specific numbers and side bets from a huge range of draws!</p>
-                    <a href="numbers<?=$ext?>" class="button button--green">Numbers demo</a>
+                    <a href="hfg<?=$ext?>" class="button button--green">Numbers demo</a>
                 </div>
             </div>
         </div>
--- menu.php
+++ menu.php
@@ -8,7 +8,7 @@
                         <a href="about-us<?=$ext?>">About Us</a>
                         <div class="header__submenu">
                             <a href="about-us<?=$ext?>#team">Meet the team</a>
-                            <a href="awards<?=$ext?>">Awards</a>
+                            <!-- <a href="awards<?=$ext?>">Awards</a> -->
                             <a href="deployments<?=$ext?>">Example Deployments</a>
                         </div>
                     </li>
@@ -16,15 +16,15 @@
                         <a href="prize-cover<?=$ext?>">Prize Cover</a>
                     </li>
                     <li>
-                        <a href="content<?=$ext?>">Content</a>
+                        <a href="games<?=$ext?>">Games</a>
                         <div class="header__submenu">
-                            <a href="lotto-betting<?=$ext?>">Lotto betting</a>
-                            <a href="instant-win-games<?=$ext?>">Instant win games</a>
-                            <a href="numbers<?=$ext?>">Numbers</a>
+                            <a href="lotto-betting<?=$ext?>">Lotto</a>
+                            <a href="instant-win-games<?=$ext?>">Instant</a>
+                            <a href="hfg<?=$ext?>">High Frequency</a>
                         </div>
                     </li>
                     <li>
-                        <a href="promotions<?=$ext?>">Promotions</a>
+                        <a href="systems<?=$ext?>">Systems</a>
                     </li>
                     <!--li>
                         <a href="lottery<?=$ext?>">Lottery Systems</a>
