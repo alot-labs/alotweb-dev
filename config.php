cbcbf3c96d4793c51245da3abf5d4ab24baadad6
Bamboo <bamboo@dreamit.sk>
2019-11-25 14:55:06 +0100

    build AW-WEB-JOB1

--- config.php
+++ config.php
@@ -1,8 +1,5 @@
-<?php 
-// extension attached to URL links
-// $ext = ".php"; // if you use php server for testing
-$ext = ".php"; // if you use html server that does not support rewriting, e.g. Web Server Chrome Extension
-// $ext = ""; // no extension for live production deployement
-
+<?php  
+$ext = ""; 
 $url = "http://129secret47719.alotops.com";
-?>
+?>;
+
